91874b141e6154eafd49f53462eb3c5d8b4716d9
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-11-24 13:38:26 -0300

    Criação das classes modelo

---
+++ src/br/ufrn/imd/modelo/Musica.java
@@ -0,0 +1,24 @@
+package br.ufrn.imd.modelo;
+
+import java.nio.file.Path;
+
+public class Musica {
+	private String nome;
+	private Path caminho;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Path getCaminho() {
+		return caminho;
+	}
+
+	public void setCaminho(Path caminho) {
+		this.caminho = caminho;
+	}
+}
---
+++ src/br/ufrn/imd/modelo/Usuario.java
@@ -0,0 +1,7 @@
+package br.ufrn.imd.modelo;
+
+public class Usuario {
+	protected Integer id;
+	protected String login;
+	protected String nome;
+}
