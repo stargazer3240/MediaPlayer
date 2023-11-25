4659efd5a8eb944f6a09603cb7f3a55085a11c7f
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-11-25 20:30:07 -0300

    Pequenas mudanças no módulo

--- src/module-info.java
+++ src/module-info.java
@@ -6,7 +6,4 @@ module MediaPlayer {
 	opens br.ufrn.imd to javafx.fxml;
 	//opens br.ufrn.imd.controle to javafx.fxml;
 	exports br.ufrn.imd;
-	// exports br.ufrn.imd.controle;
-	exports br.ufrn.imd.dao;
-	exports br.ufrn.imd.modelo;
 }
