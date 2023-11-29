9a8246ad2a4b5eebaea4a2b09f38c1742e3574ca
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-11-29 20:53:09 -0300

    Mudanças na tela principal e de login

---
+++ src/br/ufrn/imd/controle/TelaLoginController.java
@@ -0,0 +1,26 @@
+package br.ufrn.imd.controle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class TelaLoginController {
+
+	@FXML
+	private Button btnCadastro;
+
+	@FXML
+	private Button btnLogin;
+
+	@FXML
+	private Label lblFeedback;
+
+	@FXML
+	private PasswordField txtSenha;
+
+	@FXML
+	private TextField txtUsuario;
+
+}
